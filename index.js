ae2daf278dfff8daacbc82e55fdc7d0699e5f13e
Cat63730 <94469220+Cat63730@users.noreply.github.com>
2022-12-05 14:08:26 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const information = require("./information");
+
+const cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text: "i'm a module",
+    e : "oX",
+    T : "UU"
+}));
+
---
+++ information.js
@@ -0,0 +1,6 @@
+const me = {myName:"cathy", myCampus:"lyon"};
+
+module.exports = {
+    myName: me.myName,
+    myCampus: me.myCampus,
+};
